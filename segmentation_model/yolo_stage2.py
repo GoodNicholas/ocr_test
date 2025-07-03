5a32d466ac900c29fdb4cae8d06b759e1f7676c3
GoodNicholas <Nicholas.crot@yandex.com>
2025-07-03 09:45:46 +0300

    Update yolo_stage2.py

--- segmentation_model/yolo_stage2.py
+++ segmentation_model/yolo_stage2.py
@@ -1,14 +1,15 @@
 from ultralytics import YOLO
 
-model = YOLO("/content/ocr_test/yolo11x.pt")
+model = YOLO("/content/drive/MyDrive/Работа/yolo11x.pt")
 
 results = model.train(
     data="/content/ocr_test/segmentation_model/yolo_stage2.yaml",
     epochs=100,
     imgsz=640,
     augment=True,
-    batch=14,
-    device="cuda"
+    batch=40,
+    device="cuda",
+    pretrained=False
 )
 
 predictions = model("/content/ocr_test/dataset_small/images/passport_000000.png")
